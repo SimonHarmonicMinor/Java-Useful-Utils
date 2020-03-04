b069d903814624ee97937a9a12292e007bbbb32c
Semyon Kirekov <kirekov@gmail.com>
2020-03-04 23:16:26 +0700

    added ```startMeasuringInSeconds``` for ```Profiler```

--- src/test/java/com/github/simonharmonicminor/juu/measure/ProfilerTest.java
+++ src/test/java/com/github/simonharmonicminor/juu/measure/ProfilerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static com.github.simonharmonicminor.juu.measure.MeasureConverter.millisToNanos;
 import static org.awaitility.Awaitility.await;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ProfilerTest {
     private static final int DELTA_MILLIS = 100;
@@ -14,34 +14,15 @@ class ProfilerTest {
     void ifDefinedMeasureUnitIsCorrect() {
         Profiler profilerMillis = Profiler.startMeasuringInMillis();
         Profiler profilerNanos = Profiler.startMeasuringInNanos();
+        Profiler profilerSeconds = Profiler.startMeasuringInSeconds();
 
         assertEquals(MeasureUnit.MILLIS, profilerMillis.getMeasureUnit());
         assertEquals(MeasureUnit.NANOS, profilerNanos.getMeasureUnit());
+        assertEquals(MeasureUnit.SECONDS, profilerSeconds.getMeasureUnit());
     }
 
     @Test
-    void doesReturnSpecialConstantWhenStillMeasuring() {
-        Profiler profilerMillis = Profiler.startMeasuringInMillis();
-        Profiler profilerNanos = Profiler.startMeasuringInNanos();
-
-        assertEquals(Profiler.STILL_MEASURING, profilerMillis.getTime());
-        assertEquals(Profiler.STILL_MEASURING, profilerNanos.getTime());
-    }
-
-    @Test
-    void whenMeasuringIsCompleteStopAndGetReturnTheSame() {
-        Profiler profilerMillis = Profiler.startMeasuringInMillis();
-        Profiler profilerNanos = Profiler.startMeasuringInNanos();
-
-        long stoppedMillisResult = profilerMillis.stopMeasuring();
-        long stoppedNanosResult = profilerNanos.stopMeasuring();
-
-        assertEquals(stoppedMillisResult, profilerMillis.getTime());
-        assertEquals(stoppedNanosResult, profilerNanos.getTime());
-    }
-
-    @Test
-    void doMultiplyCallsStopReturnTheSame() {
+    void multipleStopsReturnTheSame() {
         Profiler profilerMillis = Profiler.startMeasuringInMillis();
         long millisTime1 = profilerMillis.stopMeasuring();
         long millisTime2 = profilerMillis.stopMeasuring();
@@ -50,23 +31,37 @@ class ProfilerTest {
         long nanosTime1 = profilerNanos.stopMeasuring();
         long nanosTime2 = profilerNanos.stopMeasuring();
 
+        Profiler profilerSeconds = Profiler.startMeasuringInSeconds();
+        long secondsTime1 = profilerSeconds.stopMeasuring();
+        long secondsTime2 = profilerSeconds.stopMeasuring();
+
         assertEquals(millisTime1, millisTime2);
         assertEquals(nanosTime1, nanosTime2);
+        assertEquals(secondsTime1, secondsTime2);
     }
 
     @Test
     void ifMeasuringInMillisIsCorrect() {
         Profiler profilerMillis = Profiler.startMeasuringInMillis();
         await().pollDelay(Duration.ONE_SECOND).until(() -> true);
-        profilerMillis.stopMeasuring();
-        assertEquals(1000, profilerMillis.getTime(), DELTA_MILLIS);
+        long res = profilerMillis.stopMeasuring();
+        assertEquals(1000, res, DELTA_MILLIS);
     }
 
     @Test
     void ifMeasuringInNanosIsCorrect() {
         Profiler profilerMillis = Profiler.startMeasuringInNanos();
         await().pollDelay(Duration.ONE_SECOND).until(() -> true);
-        profilerMillis.stopMeasuring();
-        assertEquals(millisToNanos(1000), profilerMillis.getTime(), millisToNanos(DELTA_MILLIS));
+        long res = profilerMillis.stopMeasuring();
+        assertEquals(millisToNanos(1000), res, millisToNanos(DELTA_MILLIS));
     }
+
+    @Test
+    void ifMeasuringInSecondsIsCorrect() {
+        Profiler profilerSeconds = Profiler.startMeasuringInSeconds();
+        await().pollDelay(Duration.ONE_SECOND).until(() -> true);
+        long res = profilerSeconds.stopMeasuring();
+        assertEquals(1, res);
+    }
+
 }
