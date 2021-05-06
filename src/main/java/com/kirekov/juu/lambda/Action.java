c6e878eb477ed8dc585f4dbf392c8e6909213c42
Semyon Kirekov <semyon@kirekov.com>
2021-05-06 13:43:55 +0300

    [#32] - Fixed Action class according to defined rules in checkstyle.xml

--- src/main/java/com/kirekov/juu/lambda/Action.java
+++ src/main/java/com/kirekov/juu/lambda/Action.java
@@ -1,7 +1,7 @@
 package com.kirekov.juu.lambda;
 
 /**
- * Represents a procedure, that do some stuff and returns nothing
+ * Represents a procedure that do some stuff and returns nothing.
  *
  * @since 1.0
  */
