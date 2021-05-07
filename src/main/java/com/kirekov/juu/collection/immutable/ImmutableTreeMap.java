3d5ba6247a58b18cb2ee5b526ad9e6f9a2935f02
Semyon Kirekov <semyon@kirekov.com>
2021-05-07 22:10:23 +0300

    [#41] - ImmutableTreeMap: removed @SuppressWarning("serial")

--- src/main/java/com/kirekov/juu/collection/immutable/ImmutableTreeMap.java
+++ src/main/java/com/kirekov/juu/collection/immutable/ImmutableTreeMap.java
@@ -25,7 +25,6 @@ import java.util.function.Supplier;
  * @see TreeMap
  * @since 1.1
  */
-@SuppressWarnings("serial")
 public class ImmutableTreeMap<K, V> implements ImmutableNavigableMap<K, V>, Serializable {
 
   private final TreeMap<K, V> treeMap;
