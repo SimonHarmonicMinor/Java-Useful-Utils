d282a9dba67f3b43702655b3d0bc0fe0f1a85c9b
Semyon Kirekov <semyon@kirekov.com>
2021-05-11 20:24:43 +0300

    [#49] - Try: removed unused imports

--- src/main/java/com/kirekov/juu/monad/Try.java
+++ src/main/java/com/kirekov/juu/monad/Try.java
@@ -4,14 +4,12 @@ import com.kirekov.juu.collection.Streaming;
 import com.kirekov.juu.exception.EmptyContainerException;
 import com.kirekov.juu.lambda.CheckedFunction;
 import com.kirekov.juu.lambda.CheckedSupplier;
-import java.util.Arrays;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
