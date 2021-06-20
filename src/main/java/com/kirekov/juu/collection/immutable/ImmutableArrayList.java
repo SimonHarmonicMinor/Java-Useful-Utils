df81c7ec92218677e621fdc1ac8d9d6bcafe8cce
Semyon Kirekov <semyon@kirekov.com>
2021-06-20 16:01:59 +0300

    [#75] - Removed redundant ImmutabList implementation

--- src/main/java/com/kirekov/juu/collection/immutable/ImmutableArrayList.java
+++ src/main/java/com/kirekov/juu/collection/immutable/ImmutableArrayList.java
@@ -24,7 +24,7 @@ import java.util.stream.Stream;
  * @see ArrayList
  * @since 1.0
  */
-public class ImmutableArrayList<T> extends AbstractImmutableList<T> implements ImmutableList<T> {
+public class ImmutableArrayList<T> extends AbstractImmutableList<T> {
 
   private final ArrayList<T> arrayList;
 
