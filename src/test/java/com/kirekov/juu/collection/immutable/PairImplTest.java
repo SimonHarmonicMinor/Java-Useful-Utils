b3382c9ddf1a42e47a8b9e51d8d0b6703bd33838
Semyon Kirekov <semyon@kirekov.com>
2021-06-20 17:56:02 +0300

    [#81] - Added unit test for 'PairImpl.equals'

--- src/test/java/com/kirekov/juu/collection/immutable/PairImplTest.java
+++ src/test/java/com/kirekov/juu/collection/immutable/PairImplTest.java
@@ -1,6 +1,7 @@
 package com.kirekov.juu.collection.immutable;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -22,6 +23,38 @@ class PairImplTest {
     Pair<Integer, String> pair1 = new PairImpl<>(key, value);
     Pair<Integer, String> pair2 = new PairImpl<>(key, value);
     assertEquals(pair1, pair2);
+  }
+
+  @Test
+  void equalsShouldReturnFalseIfKeysAreNotTheSame() {
+    int key = 24124;
+    String value = "fsdaf";
+    Pair<Integer, String> pair1 = new PairImpl<>(key, value);
+    Pair<Integer, String> pair2 = new PairImpl<>(key + 1, value);
+    assertNotEquals(pair1, pair2);
+  }
+
+  @Test
+  void equalsShouldReturnFalseIfValuesAreNotTheSame() {
+    int key = 24124;
+    String value = "fsdaf";
+    Pair<Integer, String> pair1 = new PairImpl<>(key, value);
+    Pair<Integer, String> pair2 = new PairImpl<>(key, value + "414");
+    assertNotEquals(pair1, pair2);
+  }
+
+  @Test
+  void equalsShouldReturnFalseIfKeysAndValuesAreNotTheSame() {
+    int key = 24124;
+    String value = "fsdaf";
+    Pair<Integer, String> pair1 = new PairImpl<>(key, value);
+    Pair<Integer, String> pair2 = new PairImpl<>(key + 123, value + "414");
+    assertNotEquals(pair1, pair2);
+  }
+
+  @Test
+  void equalsShouldReturnFalseIfComparingWithDifferentClass() {
+    assertNotEquals(new PairImpl<>("1","341"), "1");
   }
 
   @Test
