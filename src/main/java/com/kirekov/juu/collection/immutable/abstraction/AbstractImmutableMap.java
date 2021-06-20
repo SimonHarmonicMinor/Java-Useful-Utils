1347a5d6235d36aec1f91eb85e3a69a07f99c35f
Semyon Kirekov <semyon@kirekov.com>
2021-06-20 22:56:18 +0300

    [#93] - Moved all methods from AbstractImmutableMap to ImmutableMap as default interface methods

--- src/main/java/com/kirekov/juu/collection/immutable/abstraction/AbstractImmutableMap.java
+++ src/main/java/com/kirekov/juu/collection/immutable/abstraction/AbstractImmutableMap.java
@@ -1,11 +1,6 @@
 package com.kirekov.juu.collection.immutable.abstraction;
 
-import com.kirekov.juu.collection.immutable.ImmutableHashMap;
 import com.kirekov.juu.collection.immutable.ImmutableMap;
-import com.kirekov.juu.collection.immutable.Pair;
-import com.kirekov.juu.lambda.TriFunction;
-import java.util.Map;
-import java.util.Objects;
 
 /**
  * Abstract immutable map.
@@ -15,34 +10,4 @@ import java.util.Objects;
  */
 public abstract class AbstractImmutableMap<K, V> implements ImmutableMap<K, V> {
 
-  @Override
-  public boolean containsPair(Pair<K, V> pair) {
-    Objects.requireNonNull(pair);
-    return pairSet().contains(pair);
-  }
-
-  @Override
-  public ImmutableMap<K, V> concatWithOverride(ImmutableMap<K, V> map) {
-    return concatWith(map, (key, oldVal, newVal) -> newVal);
-  }
-
-  @Override
-  public ImmutableMap<K, V> concatWithoutOverride(ImmutableMap<K, V> map) {
-    return concatWith(map, (key, oldVal, newVal) -> oldVal);
-  }
-
-  @Override
-  public ImmutableMap<K, V> concatWith(ImmutableMap<K, V> mapToConcatWith,
-      TriFunction<K, V, V, V> overrideBehaviour) {
-    Map<K, V> resultMutableMap = toMutableMap();
-    mapToConcatWith.forEach(
-        (k, v) -> {
-          if (this.containsKey(k)) {
-            resultMutableMap.put(k, overrideBehaviour.apply(k, this.get(k), v));
-          } else {
-            resultMutableMap.put(k, v);
-          }
-        });
-    return new ImmutableHashMap<>(resultMutableMap);
-  }
 }
