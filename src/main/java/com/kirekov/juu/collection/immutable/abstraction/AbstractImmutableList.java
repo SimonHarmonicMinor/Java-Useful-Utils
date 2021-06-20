9a6d2c62724310a0dcffc28f5847977648d00e14
Semyon Kirekov <semyon@kirekov.com>
2021-06-20 15:55:51 +0300

    [#75] - Added AbstractImmutableList

---
+++ src/main/java/com/kirekov/juu/collection/immutable/abstraction/AbstractImmutableList.java
@@ -0,0 +1,58 @@
+package com.kirekov.juu.collection.immutable.abstraction;
+
+import com.kirekov.juu.collection.immutable.ImmutableList;
+import java.util.Objects;
+
+/**
+ * Abstract immutable list.
+ *
+ * @param <T> the type of the content
+ */
+public abstract class AbstractImmutableList<T> implements ImmutableList<T> {
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == this) {
+      return true;
+    }
+    if (!(o instanceof ImmutableList)) {
+      return false;
+    }
+    ImmutableList<?> other = (ImmutableList<?>) o;
+    if (size() != other.size()) {
+      return false;
+    }
+    for (int i = 0; i < size(); i++) {
+      if (!Objects.equals(get(i), other.get(i))) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    int hashCode = 1;
+    for (T element : this) {
+      hashCode = 31 * hashCode + (element == null ? 0 : element.hashCode());
+    }
+    return hashCode;
+  }
+
+  @Override
+  public String toString() {
+    if (isEmpty()) {
+      return "[]";
+    }
+    StringBuilder builder = new StringBuilder().append("[");
+    for (int i = 0; i < size(); i++) {
+      builder.append(get(i));
+      if (i == size() - 1) {
+        builder.append("]");
+      } else {
+        builder.append(", ");
+      }
+    }
+    return builder.toString();
+  }
+}
