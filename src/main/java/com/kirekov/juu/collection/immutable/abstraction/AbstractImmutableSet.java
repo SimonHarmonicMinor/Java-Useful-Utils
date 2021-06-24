745f1d120376237cbf02f442e115b7588097ad6d
Semyon Kirekov <semyon@kirekov.com>
2021-06-24 21:53:12 +0300

    [#111] - imports optimized

--- src/main/java/com/kirekov/juu/collection/immutable/abstraction/AbstractImmutableSet.java
+++ src/main/java/com/kirekov/juu/collection/immutable/abstraction/AbstractImmutableSet.java
@@ -2,7 +2,6 @@ package com.kirekov.juu.collection.immutable.abstraction;
 
 import com.kirekov.juu.collection.immutable.ImmutableSet;
 import java.util.Iterator;
-import java.util.List;
 
 /**
  * Abstract immutable set.
